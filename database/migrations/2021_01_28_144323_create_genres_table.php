9f690f64456fb21e47c7b64aff22327e53f5093c
Timpan4 <70634109+Timpan4@users.noreply.github.com>
2021-01-28 16:01:15 +0100

    initial commit

---
+++ database/migrations/2021_01_28_144323_create_genres_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Create genre table with id timestamp and name of the genre
+        Schema::create('genres', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genres');
+    }
+}
