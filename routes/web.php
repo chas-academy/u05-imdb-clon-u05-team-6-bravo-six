d87e5b4f227ec50b2838420cb85d78e6ac81b36f
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-01-28 14:45:19 +0100

    tinker, bootstrap and bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
