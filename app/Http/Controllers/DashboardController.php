b640ab130f4a0a4561c11cf6f6bd4f1c905c82d7
Alex <alex.soderberg97@gmail.com>
2021-02-18 00:22:17 +0100

    no error but no data on getting title, controller cant find class Title

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,24 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Models\Title;
 
 class DashboardController extends Controller
 {
     public function index(){
-        //didn't succed in sending variables defined below, will try later with more test data
-        $userData = [
+        //didn't succed in sending variables defined below to view w/o error, will try later with more test data
+        $userData = [ //supposed to later be used to show recent activity, sorted by timestamp. Will this solution work, i honestly don't know
             'watchlists' => Auth::user()->watchlists(),
             'reviews' => Auth::user()->reviews(),
             'comments' => Auth::user()->comments()
         ];
-        $userWatchlists = ['watchlists' => Auth::user()->watchlists()];
-        $userReviews =['reviews' => Auth::user()->reviews()];
-        $userComments = ['comments' => Auth::user()->comments()];
+        $userWatchlists = ['watchlists' => Auth::user()->watchlists()]; //used to show watchlists
+        $userReviews =['reviews' => Auth::user()->reviews()]; //used to show reviews
+        $userComments = ['comments' => Auth::user()->comments()]; //used to show comments
+
+        
 
         return view('dashboard', 
         ['reviews' => Auth::user()->reviews()],
+        ['comments' => Auth::user()->comments()],
         ['watchlists' => Auth::user()->watchlists()],
-        ['comments' => Auth::user()->comments()]
+        ['titles' => Title::all()]
+        
+        
         );
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,29 +4,37 @@
                 <h1 class="text-center">Welcome {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}} </h1>
                 <div class="row">
                     <div class= "card col-xl-3 col-lg-4 col-md-6 col-sm-12" >
-                    <li>ABC</li>
-                    <li>DEF</li>
-                    <li>GHI</li>
-                    <li>JKL</li>
+                    <h3 class="text-center">Recent activity:</h3>
+                    <li>Here you</li>
+                    <li>will see</li>
+                    <li>recent activity</li>
+                    <li>hopfully soon</li>
                     </div>
                     
                     <div class= "card col-xl-3 col-lg-4 col-md-6 col-sm-12">
                         <h3 class="text-center">Your Reviews:</h3>
+                        </br>
+                        {{-- $title = App\Models\Review::find(1); used in attempt to solve problem below --}}
                         @foreach ($reviews as $review)
-                            <li>
-                                <p>{{$review->body}}</p>
-                                <span>Rating: {{$review->rating}} </span>
-                            </li>
-                            </br>
+                            <div>
+                            
+                                <a>{{ $review->title()->body }}</a>{{-- problem: must return a relationship instance --}}
+                            
+                                <a>{{$review->body}} - {{$review->rating}}</a> {{-- Should later serve as link to review --}}
+                                
+                            </div>
                         @endforeach
                     </div>
 
                     <div class= "card col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                       
+                    <h3 class="text-center">Your comments:</h3>
+                        @foreach ($comments as $comment)
+                            <a>{{$comment->body}}</a>
+                        @endforeach
                     </div>
 
                     <div class= "card col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                        
+                    <h3 class="text-center">Your watchlists:</h3>
                     </div>
                 </div>
             </div> 
