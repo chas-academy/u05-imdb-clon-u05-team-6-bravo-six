4249ee3cd6a7422ef3157f82dafae16cca3cfba7
Alex <alex.soderberg97@gmail.com>
2021-02-17 14:18:30 +0100

    inital commit, already broken?

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,7 @@
-<x-app-layout>
+
+
+
+<!-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -10,8 +13,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
+                    
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </x-app-layout>
+<p>testttaar</p>
+
+
