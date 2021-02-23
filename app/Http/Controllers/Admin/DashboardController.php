330feb75294e3a0d88dbe56e75319e9ecfbead77
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-02-23 07:07:11 +0100

    finalizes an early version/draft of admin dashboard. also switches some stuff around in the headers

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Title;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        return view('admin.dashboard', ['titles' => Title::all()->sortByDesc('updated_at')]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.admin')
 @section('content')
+<h4>Recent activity</h4>
+<div class="container col-4">
+    <h5>Titles</h5>
+@foreach ($titles as $title)
+    <li class="card px-1"><div class="card-body">
+    <a href=" {{action([\App\Http\Controllers\Admin\TitleController::class, 'show'], ['title' => $title->id])}} "><h6>{{$title->title}}</h6></a>
+    <small>Updated at {{$title->updated_at}} </small>    
+    </div></li>
+@endforeach
+</div>
 
-    
+
 @endsection
