3bfd726ee3569b93c8782dee90eed77835ceab99
Alex <alex.soderberg97@gmail.com>
2021-02-23 13:34:42 +0100

    now shows username and watchlist name in watchlist.show

--- resources/views/watchlists/show.blade.php
+++ resources/views/watchlists/show.blade.php
@@ -2,10 +2,14 @@
 
 @section('content')
 {{-- <ul> --}}
+    <h1>{{$watchlist->name}}</h1>
+    <h4 class="text-mutet">By: {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</h4>
     @foreach ($watchlistItems as $item)
-    <a href="{{action([App\Http\Controllers\TitleController::class, 'show'], ['title'=>$item->title_id])}}">
-        {{$item->title()->title}} 
-    </a>
+        <div class="card">
+            <a href="{{action([App\Http\Controllers\TitleController::class, 'show'], ['title'=>$item->title_id])}}">
+                {{$item->title()->title}} 
+            </a>
+        </div>
     @endforeach
 
 @endsection
