ecb6339f300be1e54c0080b35352d16b7825cb5c
Alex <alex.soderberg97@gmail.com>
2021-03-02 15:20:39 +0100

    now admin.watchlists.show displays all watchlistItems connected to watchlist

--- resources/views/admin/watchlists/show.blade.php
+++ resources/views/admin/watchlists/show.blade.php
@@ -17,6 +17,17 @@
         <label>User ID: </label>
         <input class="form-control" name="user_id" disabled value="{{$watchlist->user_id}}">
     </div>
+    <h3>Contains Titles:</h3>
+    <ul>
+        @foreach($watchlist->watchlistItems() as $item)
+            <li>
+                <a href="{{action([\App\Http\Controllers\Admin\TitleController::class, 'show'], ['title' => $item->title()->id])}}">
+                    {{$item->title()->title}}
+                </a> 
+                with id {{$item->title()->id}}
+            </li>
+        @endforeach
+    </ul>
     <button class="btn btn-secondary btn-lg" type="submit" value="Update">Save changes</button>
 </form>
     <form method="POST" action="{{action([\App\Http\Controllers\Admin\WatchlistController::class, 'destroy'], ['watchlist' => $watchlist->id])}}">
