4e9b8a226e64160e3bacefb751b4816ee12ff0ef
Alex <alex.soderberg97@gmail.com>
2021-03-03 16:34:56 +0100

    admin.watchlists.index now show amount of items in each list

--- resources/views/admin/watchlists/index.blade.php
+++ resources/views/admin/watchlists/index.blade.php
@@ -8,7 +8,7 @@
         <th><a href="{{action([\App\Http\Controllers\Admin\WatchlistController::class, 'index'], ['sort' => 'name'])}}">Name</a></th>
         <th><a href="{{action([\App\Http\Controllers\Admin\WatchlistController::class, 'index'], ['sort' => 'user_id'])}}">User ID</a></th>
         <th><a href="{{action([\App\Http\Controllers\Admin\WatchlistController::class, 'index'], ['sort' => 'created_at'])}}">Created</a></th>
-        
+        <th>No. of items</th>
     </tr>
 </thead>
 <tbody>
@@ -18,6 +18,7 @@
     <td>{{$list->name}}</td>
     <td>{{$list->user_id}}</td>
     <td>{{$list->created_at}}</td>
+    <td class="text-center">{{$list->watchlistitems()->count()}}</td>
     <td><a href="{{action([\App\Http\Controllers\Admin\WatchlistController::class, 'show'], ['watchlist' => $list->id])}}">View</a></td>
 
 </tr>
