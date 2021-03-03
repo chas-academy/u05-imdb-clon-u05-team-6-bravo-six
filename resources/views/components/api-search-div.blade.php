030f2023c2ce459ff44988c0d45d995400ccde9b
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-03-03 10:19:43 +0100

    removed commented out script from view, created new component ApiSearchDiv and moved template functionality into it

---
+++ resources/views/components/api-search-div.blade.php
@@ -0,0 +1,13 @@
+<div>
+<div class="form-group">
+    <label>{{$title}}</label>
+    
+    <input id="search-input" class="form-control">
+    <span id="search-clear" class="btn btn-warning">Clear</span>
+    <span  class=" alert-danger" id="status"></span>
+    
+</div>
+    
+    <div id="search-results">
+    </div>
+</div>
