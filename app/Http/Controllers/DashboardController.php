b161e89dbda5dcd1c25ef43032f8ff96f36eedb7
Otto Reimers <70694917+ottoreimers@users.noreply.github.com>
2021-03-04 14:26:57 +0100

    now the profile pic shows in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Auth;
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
                                 </li>
                             @endif
                         @else
-
+                        <x-image-layout :user="Auth::user()"></x-image-layout>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
