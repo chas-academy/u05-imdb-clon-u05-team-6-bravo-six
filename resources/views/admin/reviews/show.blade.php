e27ce4892abd7ae9b770cb383e6f1f9e2b521aa4
Johan <johan.engstrom@chasacademy.se>
2021-03-05 14:45:34 +0100

    Linked title and reviewer/user under relationship on show just as on overview

--- resources/views/admin/reviews/show.blade.php
+++ resources/views/admin/reviews/show.blade.php
@@ -6,13 +6,10 @@
     @csrf
     @method('PUT')
     <div class="form-group">
-        <label>Review for Title:</label>
-        <input class="form-control" disabled value="{{$review->title()->title}}">
-    </div>
-    <div class="form-group">
         <label>Review Title:</label>
         <input class="form-control" name="title" value="{{$review->title}}">
     </div>
+
     <div class="form-group">
         <label>Review:</label>
         <input class="form-control" name="body" value="{{$review->body}}">
@@ -22,16 +19,11 @@
         <label>Created at: </label>
         <input class="form-control" disabled value="{{$review->created_at}}">
     </div>
+
     <div class="form-group">
         <label>Updated at: </label>
         <input class="form-control" disabled value="{{$review->updated_at}}">
     </div>
-    <div class="form-group">
-        <label>Posted by user: </label>
-        <input class="form-control" disabled value="{{$review->user()->name}}">
-    </div>
-
-
 </form>
     <input form="update" class="btn btn-secondary btn-lg" type="submit" value="Update review">
     <input form="delete" class="btn btn-danger btn-lg" type="submit" value="Delete review">
@@ -39,4 +31,19 @@
         @csrf
         @method('DELETE')
     </form>
+    <div class="container px-6" style="margin-top: 1em">
+        <h3>Relationships</h3>
+
+        <div class="form-group">
+            <label>Review for Title:</label>
+            <input class="form-control" disabled value="{{$review->title()->title}}">
+            <a href="{{action([\App\Http\Controllers\Admin\TitleController::class, 'show'], ['title' => $review->title()->id])}}">Go to title:</a>
+        </div>
+
+        <div class="form-group">
+            <label>Posted by user: </label>
+            <input class="form-control" disabled value="{{$review->user()->name}}">
+            <a href="{{action([\App\Http\Controllers\Admin\UserController::class, 'show'], ['user' => $review->user()->id])}}">Go to user:</a>
+        </div>
+    </div>           
 @endsection
