5c9159f18313987e5286399b79cd7bc4610b31b6
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-03-18 10:42:54 +0100

    changed it back, removed cap of amounts of genres displayed

--- resources/views/components/navigation-aside.blade.php
+++ resources/views/components/navigation-aside.blade.php
@@ -8,17 +8,11 @@
         </li>
         @endauth
         <h5 class="border-bottom mt-2">Genres</h5>
-        @if ($currentPage)
-        <?php $currentGenre = \App\Models\Genre::find($currentPage);?>
-        <li><a class=" active font-weight-bold breadcrumb-item" href="{{action([\App\Http\Controllers\GenreController::class, 'show'], ['genre' => $currentGenre->id])}}">{{$currentGenre->name}}</a></li>
-        @endif
-        @foreach(\App\Models\Genre::all()->take(7) as $genre)
-        @if(!($currentPage === $genre->id))
-        <li>
-            <a class="breadcrumb-item" href="{{action([\App\Http\Controllers\GenreController::class, 'show'], ['genre' => $genre->id])}}">{{$genre->name}}</a></li>
-        @endif
+        @foreach(\App\Models\Genre::all() as $genre)
+
+        <li><a class="@if($currentPage === $genre->id) active font-weight-bold @endif breadcrumb-item
+            " href="{{action([\App\Http\Controllers\GenreController::class, 'show'], ['genre' => $genre->id])}}">{{$genre->name}}</a></li>
         @endforeach
-        <li><a href="{{action([\App\Http\Controllers\GenreController::class, 'index'])}}">...</a></li>
     </ul>
 </aside>
 </div>
