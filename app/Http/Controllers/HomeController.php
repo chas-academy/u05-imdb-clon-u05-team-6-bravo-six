9e60eaf0bc9223ebd57256f50411bf8002a9f473
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-03-22 13:53:47 +0100

    fix for auth bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,10 +16,6 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
@@ -29,30 +25,30 @@ class HomeController extends Controller
     public function index()
     {
         $movies = Title::all();
-        
+
         $topMovies = array(0, 0, 0);
         $mov1 = null;
         $mov2 = null;
         $mov3 = null;
-        foreach($movies as $movie){
-            if( $topMovies[2] < $movie->avgRating() && $topMovies[1] < $movie->avgRating() && $topMovies[0] < $movie->avgRating()){
+        foreach ($movies as $movie) {
+            if ($topMovies[2] < $movie->avgRating() && $topMovies[1] < $movie->avgRating() && $topMovies[0] < $movie->avgRating()) {
                 $topMovies[0] = $movie->avgRating();
-                $mov1=$movie;
-            }elseif($movie->avgRating() > $topMovies[2] && $movie->avgRating() > $topMovies[1]){
+                $mov1 = $movie;
+            } elseif ($movie->avgRating() > $topMovies[2] && $movie->avgRating() > $topMovies[1]) {
                 $topMovies[1] = $movie->avgRating();
-                $mov2=$movie;
-            }elseif($movie->avgRating() > $topMovies[2]){
+                $mov2 = $movie;
+            } elseif ($movie->avgRating() > $topMovies[2]) {
                 $topMovies[2] = $movie->avgRating();
-                $mov3=$movie;
+                $mov3 = $movie;
             }
         }
-        
+
         $reviews = Review::withCount('commentsQuery')->get();
         $topReviews = $reviews->sortByDesc('comments_query_count')->take(5);
-        
-        
-        
-        
-        return view('welcome' ,['mov1'=> $mov1, 'mov2'=> $mov2, 'mov3'=> $mov3, 'topReviews' => $topReviews]);
+
+
+
+
+        return view('welcome', ['mov1' => $mov1, 'mov2' => $mov2, 'mov3' => $mov3, 'topReviews' => $topReviews]);
     }
 }
