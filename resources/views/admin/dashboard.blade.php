9609880f66bd1d8d89802d0df81e69d8d4e27178
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-03-22 16:06:20 +0100

    adds basic dashboard, maybe done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.admin')
 @section('content')
-<h4>Recent activity</h4>
-<div class="container col-4">
-    <h5>Titles</h5>
-@foreach ($titles as $title)
-    <li class="card px-1"><div class="card-body">
-    <a href=" {{action([\App\Http\Controllers\Admin\TitleController::class, 'show'], ['title' => $title->id])}} "><h6>{{$title->title}}</h6></a>
-    <small>Updated at {{$title->updated_at}} </small>    
-    </div></li>
-@endforeach
+<h1>Welcome to our admin-panel!</h1>
+<p class="lead">
+    Use this to control all content added to the site. 
+    Use either the links below or above, in the header, to navigate.
+    To get back to the regular site, click U05-bravo-six in the header or just <a href="{{route('home')}}">here!</a>
+</p>
+<div class="row">
+    <div class="p-2  col-md-4 col-sm-12"><div class="card p-2"><a href="{{action([\App\Http\Controllers\Admin\TitleController::class, 'index'])}}"><h3 class="font-weight-light">Titles/Movies</h3></a></div></div>
+    <div class="p-2  col-md-4 col-sm-12"><div class="card p-2"><a href="{{action([\App\Http\Controllers\Admin\GenreController::class, 'index'])}}"><h3 class="font-weight-light">Genres</h3></a></div></div>
+    <div class="p-2  col-md-4 col-sm-12"><div class="card p-2"><a href="{{action([\App\Http\Controllers\Admin\UserController::class, 'index'])}}"><h3 class="font-weight-light">Users</h3></a></div></div>
+    <div class="p-2  col-md-4 col-sm-12"><div class="card p-2"><a href="{{action([\App\Http\Controllers\Admin\ReviewController::class, 'index'])}}"><h3 class="font-weight-light">Reviews</h3></a></div></div>
+    <div class="p-2  col-md-4 col-sm-12"><div class="card p-2"><a href="{{action([\App\Http\Controllers\Admin\CommentController::class, 'index'])}}"><h3 class="font-weight-light">Comments</h3></a></div></div>
+    <div class="p-2  col-md-4 col-sm-12"><div class="card p-2"><a href="{{action([\App\Http\Controllers\Admin\WatchlistController::class, 'index'])}}"><h3 class="font-weight-light">Watchlists</h3></a></div></div>
 </div>
-
-
 @endsection
