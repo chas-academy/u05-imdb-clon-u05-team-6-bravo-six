020d0fc6f729140ad33f1aba34583a9659dba74d
Tim Aronsson <70634109+Timpan4@users.noreply.github.com>
2021-03-23 11:21:02 +0100

    Watchlist search layout

--- resources/views/components/watchlist-card.blade.php
+++ resources/views/components/watchlist-card.blade.php
@@ -2,17 +2,24 @@
     <div class="card title-card">
          <div class="row">
          {{-- <span class="dropdown_activator"></span> --}}
-         <div class=" col-md-12">
-         {{-- </div> --}}
-         <p>{{$watchlist->name}}</p>
-         <p>{{$watchlist->name}}</p>
+         <div class=" col-md-6">
+          <a href="{{action([App\Http\Controllers\WatchlistController::class, 'show'], ["watchlist"=>$watchlist->id])}}"><h3>{{$watchlist->name}}</h3></a>
+         <p>Last updated: {{$watchlist->updated_at}}</p></div>
+          <div class="col-md-6">
+               <div class="col-md-8 float-right">
+          @if ($user->img_url !== null)
+               <img src="{{ asset('storage/' . $user->img_url) }}" alt="No photo" class="rounded-circle float-right" style="max-width: 80px">
+          @else
+               <img src="https://crestedcranesolutions.com/wp-content/uploads/2013/07/facebook-profile-picture-no-pic-avatar.jpg" alt="No profile picture" class="rounded-circle float-right" style="max-width: 80px">
+          @endif
+         <p class="float-right">Created by: {{$user->name}}</p>
          </div>
-
-         {{-- if i remove row 19-22 the aside moves around wonky  --}}
+         </div>
+         </div>
+         {{-- if i remove these 2 divs the aside moves around wonky  --}}
             <div class="dropdown col-md-3">
               <div>
                         </div>
               </div>
-         </div>
     </div>
 </div>
--- resources/views/watchlists/search.blade.php
+++ resources/views/watchlists/search.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<script>
-    console.log("hi")
-</script>
     {{-- <div class="row">
         class filter must remain on button for jquery to work
     @foreach (\App\Models\Genre::all() as $genre)
         <button class="btn btn-sm m-1 filter" data-id="{{$genre->id}}">{{$genre->name}}</button>
-        
+
     @endforeach
     </div> --}}
 <div class="navbar-nav ml-auto justify-content-end">
-            <form class="form-inline my-2 my-lg-0" action="/wsearch" method="GET" role="search">
+            <form class="form-inline my-2 my-lg-0" action="{{route('watchlists.search')}}" method="GET" role="search">
                     {{ csrf_field() }}
                     <div class="input-group">
-                        <input type="text" class="form-control mr-sm-2" placeholder="Search for..." 
+                        <input type="text" class="form-control mr-sm-2" placeholder="Search for..."
                         @if(request('q'))
                         value="{{request('q')}}"
                         @endif
@@ -33,13 +30,12 @@
             </h1>
             @foreach ($watchlists as $watchlist)
             <div class="movie">
-                {{$watchlist->user()->name}}
-                <x-watchlist-card :watchlist="$watchlist" :user="$watchlist"></x-watchlist-card>
+                <x-watchlist-card :watchlist="$watchlist" :user="$watchlist->user()"></x-watchlist-card>
             </div>
             @endforeach
         </div>
         <x-navigation-aside></x-navigation-aside>
-        
+
         {{-- <div class="row">{{$titles->appends(['q' => request('q')])->links()}}</div> --}}
     </div>
 @endsection
