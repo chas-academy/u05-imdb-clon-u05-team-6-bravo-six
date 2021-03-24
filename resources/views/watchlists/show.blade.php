232f3a80960f21b0ebaa8886bcd1e36e6d4ab5d8
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-03-24 15:48:51 +0100

    not yet!

--- resources/views/watchlists/show.blade.php
+++ resources/views/watchlists/show.blade.php
@@ -5,15 +5,15 @@
     <ol class="breadcrumb">
 <a class="breadcrumb-item" href="{{action([\App\Http\Controllers\WatchlistController::class, 'index'])}}">Watchlists</a>
 <a class="breadcrumb-item active" href="{{action([\App\Http\Controllers\WatchlistController::class, 'show'], ['watchlist' => $watchlist->id])}}">{{$watchlist->name}}</a>
-@if($watchlist->public === 1)
-<small class="text-muted">Public</small>
-@else
-<small class="text-muted">Private</small>
-@endif
     </ol>
 </nav>
     <h1 class="border-bottom">{{$watchlist->name}}</h1>
     <h4 class="text-mutet">By: {{\App\Models\User::find($watchlist->user_id)->name}}</h4>
+    @if($watchlist->public === 1)
+<small class="text-muted">Public</small>
+@else
+<small class="text-muted">Private</small>
+@endif
     <?php $deletable = Auth::id() === $watchlist->user_id; ?>
     @foreach ($watchlistItems as $item)
     <?php
