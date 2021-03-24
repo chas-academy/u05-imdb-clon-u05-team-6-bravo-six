ee23622ec9551346e5020cce6a53d2272a80475b
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-03-24 14:20:12 +0100

    edited admin credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::create([
             'name' => 'admin',
-            'email' => 'admin@admin.com',
+            'email' => 'larare@geOssVG.snalla',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('hejbjorn'), // password
             'remember_token' => Str::random(10),
             'user_admin' => true
         ]);
