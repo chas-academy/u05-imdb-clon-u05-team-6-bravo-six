15bc1bbb5cc37b7f9132539dc6fdbc972ed2ef7d
Alex <alex.soderberg97@gmail.com>
2021-03-26 13:31:06 +0100

    now uses cleaner code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,46 +21,13 @@ class HomeController extends Controller
     public function index()
     {
         $movies = Title::all();
-        //$movies->avgRating = $movies->avgRating();
-
-        //dd($movies);
-
-        $topMovies = array(0, 0, 0);
-        $mov1 = null;
-        $mov2 = null;
-        $mov3 = null;
+        
         foreach ($movies as $movie) {
-            $avgRating = $movie->avgRating();
-            if ($topMovies[2] < $avgRating && $topMovies[1] < $avgRating && $topMovies[0] <= $avgRating) {
-                
-                if($topMovies[1] !== 0 && $topMovies[2] !== 0){
-                    $topMovies[2] = $mov2->avgRating();
-                    $topMovies[1] = $mov1->avgRating();
-                    $mov3 = $mov2;
-                    $mov2 = $mov1;
-                    
-                }elseif ($topMovies[1] !== 0){
-                    $topMovies[1] = $mov1->avgRating();
-                    $mov2 = $mov1;
-                }
-                $topMovies[0] = $avgRating;
-                $mov1 = $movie;
-            } elseif ($avgRating > $topMovies[2] && $avgRating >= $topMovies[1]) {
-                
-                if($topMovies[2] !== 0 && $topMovies[1] !== 0){
-                    $topMovies[2] = $mov2->avgRating();
-                    $mov3 = $mov2;
-                    
-                }
-                $topMovies[1] = $avgRating;
-                $mov2 = $movie;
-            } elseif ($avgRating >= $topMovies[2]) {
-                $topMovies[2] = $avgRating;
-                $mov3 = $movie;
-            }
+        $movie->avgRating = $movie->avgRating();
         }
-
-
+        
+        $topMovies = $movies->sortByDesc('avgRating')->take(3);
+        
 
         $reviews = Review::withCount('commentsQuery')->get();
         $topReviews = $reviews->sortByDesc('comments_query_count')->take(5);
@@ -71,6 +38,6 @@ class HomeController extends Controller
         for ($i = 0; $i < 3; $i++) {
             array_push($randomMovies, Title::inRandomOrder()->first());
         };
-        return view('welcome', ['mov1' => $mov1, 'mov2' => $mov2, 'mov3' => $mov3, 'topReviews' => $topReviews, 'recentMovies' => $recentMovies, 'randomMovies' => $randomMovies]);
+        return view('welcome', ['topMovies' => $topMovies, 'topReviews' => $topReviews, 'recentMovies' => $recentMovies, 'randomMovies' => $randomMovies]);
     }
 }
